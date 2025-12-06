4499b9cbc7c27d968741e2ef452ca086949b39fb
santoshbilla <sansha508@yahoo.com>
2025-12-06 09:56:53 -0800

    Update

---
+++ ui-components/src/app/components/button/button.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  imports: [],
+  templateUrl: './button.html',
+  styleUrl: './button.scss',
+})
+export class Button {
+  const() {
+    console.log('Button component initialized');
+  }
+}
