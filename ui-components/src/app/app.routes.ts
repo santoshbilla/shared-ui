f0d4be9c18de523d78eb59a51203512a006ab223
santoshbilla <sansha508@yahoo.com>
2025-12-06 10:28:42 -0800

    Update

---
+++ ui-components/src/app/app.routes.ts
@@ -0,0 +1,3 @@
+import { Routes } from '@angular/router';
+import { Button } from './components/button/button';
+export const routes: Routes = [{ path: 'button', component: Button }];
---
+++ ui-components/src/app/index.ts
@@ -0,0 +1,1 @@
+export * from './components/button/shared-ui.module';
